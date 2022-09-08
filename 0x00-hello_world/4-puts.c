9650583a784ff3982a731dfa3bde58d58cfd2d8f
“eunicePops” <enablersolutions@gmail.com>
2022-09-08 13:18:15 -0700

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - entrypoint 
+ * main - entrypoint
  *
  * Return: Always o
  *
  */
 int main(void)
 {
-
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
