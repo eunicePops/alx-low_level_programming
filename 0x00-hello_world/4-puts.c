749a311b852390b1ac779f0c7a27e2b8d08901e0
“eunicePops” <enablersolutions@gmail.com>
2022-09-08 12:47:57 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entrypoint 
+ *
+ * Return: Always o
+ *
+ */
+int main(void)
+{
+
+	puts("Programming is like building a multilingual puzzle\n")
+	return(0);
+}
