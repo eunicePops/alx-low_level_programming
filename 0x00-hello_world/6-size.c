eddb208711e5aa460ffaf0acfcd3c8531ea65f49
“eunicePops” <enablersolutions@gmail.com>
2022-09-08 15:04:46 -0700

    Hot fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 /**
+ * main - entrypoint
  * @chara - holds size of char
  * @integ - Holds size of integer
  * @longInt - Holds size of long int
@@ -15,10 +16,10 @@ int main(void)
 	long long int longLongInt;
 	float floatt;
 
-	printf("Size of a char: %Iu bytes(s)\n", sizeof(chara));
-	printf("Size of an int: %Iu bytes(s)\n", sizeof(integ));
-	printf("Size of a long int: %Iu bytes(s)\n", sizeof(longInt));
-	printf("Size of a long long int: %Iu bytes(s)\n", sizeof(longLongInt));
-	printf("Size of a float: %Iu bytes(s)\n", sizeof(floatt));
+	printf("Size of a char: %d bytes(s)\n", sizeof(chara));
+	printf("Size of an int: %d bytes(s)\n", sizeof(integ));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(longInt));
+	printf("Size of a long long int: %d bytes(s)\n", sizeof(longLongInt));
+	printf("Size of a float: %d bytes(s)\n", sizeof(floatt));
 	return (0);
 }
