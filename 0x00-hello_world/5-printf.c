1bec2c096c8a4b62427e36ba4b9d0e20623758cb
“eunicePops” <enablersolutions@gmail.com>
2022-09-08 13:36:13 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entrypoint                                                                       
+ *
+ * Return: Always o                                                                        
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+                                                                                       
+
