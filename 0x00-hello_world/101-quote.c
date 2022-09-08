1514eea69370715f3fe5096a14f84396e2361133
“eunicePops” <enablersolutions@gmail.com>
2022-09-08 15:29:17 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entrypoint
+ *
+ * Return: 1 on success
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
