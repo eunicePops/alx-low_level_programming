dcd039db5f67ad029fb43a988b1a0d45624d31bd
“eunicePops” <enablersolutions@gmail.com>
2022-09-08 14:49:22 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * @chara - holds size of char
+ * @integ - Holds size of integer
+ * @longInt - Holds size of long int
+ * @longLongInt - Holds size of long long int
+ * @floatt - Holds size of float
+ * Return: always o
+ */
+int main(void)
+{
+	char chara;
+	int integ;
+	long int longInt;
+	long long int longLongInt;
+	float floatt;
+
+	printf("Size of a char: %zu bytes(s)\n", sizeof(chara));
+	printf("Size of an int: %zu bytes(s)\n", sizeof(integ));
+	printf("Size of a long int: %zu bytes(s)\n", sizeof(longInt));
+	printf("Size of a long long int: %zu bytes(s)\n", sizeof(longLongInt));
+	printf("Size of a float: %zu bytes(s)\n", sizeof(floatt));
+	return (0);
+}
