5eeda6da66f148dfaebc4e432692324d2219f3d4
“eunicePops” <enablersolutions@gmail.com>
2022-09-13 07:19:18 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - entrypoint
+ *
+ * Description - Prints _putchar as a message
+ *
+ * Return: Always 0 on (Success)
+ */
+
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+
+	int counts siz;
+
+	siz = sizeof(str) / sizeof(int);
+
+	for (counts = 0; counts < sz; counts++)
+	{
+		_putchar(str[count]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
