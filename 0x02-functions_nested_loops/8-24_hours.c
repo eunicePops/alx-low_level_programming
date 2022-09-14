827a5312acdc07f65bd1c10d979d7a2711c0ee46
“eunicePops” <enablersolutions@gmail.com>
2022-09-13 20:36:53 -0700

    natural

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -25,22 +25,26 @@ void jack_bauer(void)
 		while (hours_ones < hours_max)
 		{
 			minutes_tens = '0';
-			while (minutes_ones < 58)
+			while (minutes_tens < '6')
 			{
-				_putchar(hours_tens);
-				_putchar(hours_ones);
-				_putchar(':');
-				_putchar(minutes_tens);
-				_putchar(minutes_ones);
-				_putchar('\n');
-				minutes_ones++;
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
 			}
-			minutes_ones = '0';
-			minutes_tens++;
+			minutes_tens = '0';
+			hours_ones++;
 		}
-		minutes_tens = '0';
-		hours_ones++;
+		hours_ones = '0';
+		hours_tens++;
 	}
-	hours_ones = '0';
-	hours_tens++;
 }
