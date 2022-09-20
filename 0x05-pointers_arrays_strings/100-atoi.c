9c46e74c8192c0565a448f9cdaefebc97e5214cc
“eunicePops” <enablersolutions@gmail.com>
2022-09-19 21:04:48 -0700

    fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,23 +13,21 @@ int _atoi(char *s)
 	int a = 0, digit = 0, num = 0, signe = 0, count = 0, length = 0;
 
 	while (s[length] != '\0')
-	{
 		length++;
-	}
 	while (count < length && a == 0)
 	{
 		if (s[count] == '-')
 			++signe;
 		if (s[count] >= '0' && s[count] <= '9')
 		{
-			 digit = s[count] - '0';
-			 if (signe % 2)
-				 digit = -digit;
-			 num = num * 10 + digit;
-			 a = 1;
-			 if (s[count + 1] < '0' || s[count + 1] > '9')
-				 break;
-			 a = 0;
+			digit = s[count] - '0';
+			if (signe % 2)
+				digit = -digit;
+			num = num * 10 + digit;
+			a = 1;
+			if (s[count + 1] < '0' || s[count + 1] > '9')
+				break;
+			a = 0;
 		}
 		count++;
 	}
