e747928a247bb86e3600ab1d0b74bf6a0819908d
eunicePops <enablersolutions@gmail.com>
2023-04-03 14:43:33 -0700

    elements of a linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - is a function that prints all the elements of a linked list named listint_t
+ * @h: head of the list
+ *
+ * Return: the return is the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *indicator = h;
+	size_t tally = 0;
+
+	while (indicator != NULL)
+	{
+		printf("%d\n", indicator->n);
+		tally += 1;
+		indicator = indicator->next;
+	}
+	return (tally);
+}
