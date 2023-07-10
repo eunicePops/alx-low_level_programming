051cab67d05ac5f14a6e2c738e66da2fe6425642
eunicepops <eunice.morakinyo@gmail.com>
2023-07-10 14:22:59 -0700

    append

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,8 +6,8 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - a function that reads a text file and prints it to the POSIX standard output
- * @filename: is the name of the file to be read
+ * read_textfile -a function that  reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
  * @letters: number of letters it should read and print
  *
  * Return: actual number of letters it could read and print
